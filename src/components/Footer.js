353fa7b1e55bc625a28c0a93d3166798df723a35
Igor <igor06brw@gmail.com>
2020-12-29 17:04:43 +0100

    add links of footers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,46 @@ const Footer = () => {
         <footer>
             <div>
                 <nav>
-                
+                    <ul>
+                        <p>Information</p>
+                        <li><a href="#">The brand</a></li>
+                        <li><a href="#">Local stores</a></li>
+                        <li><a href="#">Customer service</a></li>
+                        <li><a href="#">Privacy & cookies</a></li>
+                        <li><a href="#">Site map</a></li>
+                    </ul>
+                    <ul>
+                        <p>Why buy from us</p>
+                        <li><a href="#">Shipping & returns</a></li>
+                        <li><a href="#">Secure shopping</a></li>
+                        <li><a href="#">Testimonials</a></li>
+                        <li><a href="#">Award winning</a></li>
+                        <li><a href="#">Ethical trading</a></li>
+                    </ul>
+                    <ul>
+                        <p>Your account</p>
+                        <li><a href="#">Sign in</a></li>
+                        <li><a href="#">Register</a></li>
+                        <li><a href="#">View cart</a></li>
+                        <li><a href="#">View your lookbook</a></li>
+                        <li><a href="#">Track and order</a></li>
+                        <li><a href="#">Update information</a></li>
+                    </ul>
+                    <ul>
+                        <p>Lookbook</p>
+                        <li><a href="#">Latest posts</a></li>
+                        <li><a href="#">Men's lookbook</a></li>
+                        <li><a href="#">Women's lookbook</a></li>
+                        <li><a href="#">Lookbook RSS feed</a></li>
+                        <li><a href="#">View your lookbook</a></li>
+                        <li><a href="#">Delete your lookbook</a></li>
+                    </ul>
+                    <ul>
+                        <p>Contact details</p>
+                        <li><a href="#">Head Office: Aviji Shop, 123-456 Bishop Street, Los Stantos</a></li>
+                        <li><a href="tel:0123-456-789">Telephone: 0123-456-789</a></li>
+                        <li><a href="mailto:support@avijishop.com">Email: support@avijishop.com</a></li>
+                    </ul>
                 </nav>
             </div>
             <div>
