75bd84c5db9435b00a6f6b62fbe1d0cd2dc810c6
Igor <igor06brw@gmail.com>
2020-12-30 16:33:39 +0100

    add redirect and route to auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 
 import './App.css';
 import Header from './components/Header';
 import ProductDetail from "./components/Products/ProductDetails/ProductDetails";
 import Products from './components/Products/Products';
-import Login from './components/Login';
-import Register from './components/Register';
 import Footer from "./components/Footer";
+import Auth from "./components/Auth/Auth";
 
 
 function App() {
@@ -18,8 +17,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Products} />
           <Route path="/product-detail" component={ProductDetail} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
+          <Redirect from="/register" to="/auth" />
+          <Redirect from="/login" to="/auth" />
+          <Route path="/auth" component={Auth} />
         </Switch>
         <Footer />
       </div>
