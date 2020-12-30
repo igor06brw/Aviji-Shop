fcbba489226b9c61383eb6b86ddda8db93078661
Igor <igor06brw@gmail.com>
2020-12-30 02:34:02 +0100

    finish some style of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,24 +6,8 @@ const Footer = () => {
     return (
         <footer className="">
             <div className="">
-                <nav className="d-flex flex-row">
-                    <ul className="list-group list-unstyled text-left">
-                        <p>Information</p>
-                        <li><a href="#" className="text-decoration-none">The brand</a></li>
-                        <li><a href="#" className="text-decoration-none">Local stores</a></li>
-                        <li><a href="#" className="text-decoration-none">Customer service</a></li>
-                        <li><a href="#" className="text-decoration-none">Privacy & cookies</a></li>
-                        <li><a href="#" className="text-decoration-none">Site map</a></li>
-                    </ul>
-                    <ul className="list-group list-unstyled text-left">
-                        <p>Why buy from us</p>
-                        <li><a href="#" className="text-decoration-none">Shipping & returns</a></li>
-                        <li><a href="#" className="text-decoration-none">Secure shopping</a></li>
-                        <li><a href="#" className="text-decoration-none">Testimonials</a></li>
-                        <li><a href="#" className="text-decoration-none">Award winning</a></li>
-                        <li><a href="#" className="text-decoration-none">Ethical trading</a></li>
-                    </ul>
-                    <ul className="list-group list-unstyled text-left">
+                <nav className="footer-nav d-flex flex-row">
+                    <ul className="footer-link-list list-group list-unstyled text-left">
                         <p>Your account</p>
                         <li><a href="#" className="text-decoration-none">Sign in</a></li>
                         <li><a href="#" className="text-decoration-none">Register</a></li>
@@ -32,7 +16,7 @@ const Footer = () => {
                         <li><a href="#" className="text-decoration-none">Track and order</a></li>
                         <li><a href="#" className="text-decoration-none">Update information</a></li>
                     </ul>
-                    <ul className="list-group list-unstyled text-left">
+                    <ul className="footer-link-list list-group list-unstyled text-left">
                         <p>Lookbook</p>
                         <li><a href="#" className="text-decoration-none">Latest posts</a></li>
                         <li><a href="#" className="text-decoration-none">Men's lookbook</a></li>
@@ -41,7 +25,7 @@ const Footer = () => {
                         <li><a href="#" className="text-decoration-none">View your lookbook</a></li>
                         <li><a href="#" className="text-decoration-none">Delete your lookbook</a></li>
                     </ul>
-                    <ul className="list-group list-unstyled text-left">
+                    <ul className="footer-link-list list-group list-unstyled text-left">
                         <p>Contact details</p>
                         <li><a href="#" className="text-decoration-none">Head Office: Aviji Shop, 123-456 Bishop Street, Los Stantos</a></li>
                         <li><a href="tel:0123-456-789" className="text-decoration-none">Telephone: 0123-456-789</a></li>
