0cff4e02a8ba62b9fc16a9f7edb790cb479df01c
Igor <igor06brw@gmail.com>
2020-12-30 18:39:42 +0100

    add Layout to App

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import ProductDetail from "./components/Products/ProductDetails/ProductDetails";
 import Products from './components/Products/Products';
 import Footer from "./components/Footer";
 import Auth from "./components/Auth/Auth";
+import Layout from "./Layout/Layout";
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Products} />
-          <Route path="/product-detail" component={ProductDetail} />
-          <Redirect from="/register" to="/auth" />
-          <Redirect from="/login" to="/auth" />
-          <Route path="/auth" component={Auth} />
-        </Switch>
-        <Footer />
+        <Layout>
+          <Header />
+            <Switch>
+              <Route exact path="/" component={Products} />
+              <Route path="/product-detail" component={ProductDetail} />
+              <Redirect from="/register" to="/auth" />
+              <Redirect from="/login" to="/auth" />
+              <Route path="/auth" component={Auth} />
+            </Switch>
+          <Footer />
+        </Layout>
       </div>
     </Router>
   );
