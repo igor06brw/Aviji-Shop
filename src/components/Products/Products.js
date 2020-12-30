c44894cd45ca9c162093a983167532c16b778ad5
Igor <igor06brw@gmail.com>
2020-12-30 03:23:27 +0100

    change products.js to class

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Product from './Product/Product';
 
 import '../../styles/Products.css'
 
-const Products = () => {
-    return (
-        <main className="container border-top mt-5 pl-0">
-            <nav className="nav my-3">
-                <a className="nav-link text-success" href="#">Popular</a>
-                <a className="nav-link text-success" href="#">New arrivals</a>
-                <a className="nav-link text-success" href="#">Best sellers</a>
-                <a className="nav-link text-success" href="#">Special offers</a>
-            </nav>
-            <div className="row row-cols-1 row-cols-md-3">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-            </div>
-        </main>
-    );
+class Products extends Component {
+    
+    
+    render() {
+        return (
+            <main className="container border-top mt-5 pl-0">
+                <nav className="nav my-3">
+                    <a className="nav-link text-success" href="#">Popular</a>
+                    <a className="nav-link text-success" href="#">New arrivals</a>
+                    <a className="nav-link text-success" href="#">Best sellers</a>
+                    <a className="nav-link text-success" href="#">Special offers</a>
+                </nav>
+                <div className="row row-cols-1 row-cols-md-3">
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+            </main>
+        );
+    }
 }
 
 export default Products;
