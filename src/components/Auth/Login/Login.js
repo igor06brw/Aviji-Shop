a5751dd5f7c7abc64c656bea09a1cbb244f3ff14
Igor <igor06brw@gmail.com>
2020-12-30 17:58:01 +0100

    small graphic changes, changes of class to classNAme

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -4,16 +4,16 @@ const Login = () => {
     return (
         <div className="p-5 col">
             <form>
-                <h2 className="display-6">Login</h2>
-                <div class="mb-3">
-                    <label for="loginEmail" class="form-label">Your Email...</label>
-                    <input type="email" class="form-control" id="loginEmail" required/>
+                <h2 className="display-4 pb-4">Login</h2>
+                <div className="mb-3">
+                    <label for="loginEmail" className="form-label">Your Email...</label>
+                    <input type="email" className="form-control" id="loginEmail" required/>
                 </div>
-                <div class="mb-3">
-                    <label for="loginPassword" class="form-label">Your Password...</label>
-                    <input type="password" class="form-control" id="loginPassword" />
+                <div className="mb-3">
+                    <label for="loginPassword" className="form-label">Your Password...</label>
+                    <input type="password" className="form-control" id="loginPassword" />
                 </div>
-                <button type="submit" class="btn btn-lg btn-success">Sign in</button>
+                <button type="submit" className="btn btn-lg btn-success">Sign in</button>
             </form>
         </div>
     );
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -4,25 +4,25 @@ const Register = () => {
     return (
         <div className="p-5 col">
             <form>
-                <h2>Register</h2>
-                <div class="mb-3 ">
-                    <label for="registerEmail" class="form-label">Your Email...</label>
-                    <input type="email" class="form-control" id="registerEmail" aria-describedby="emailHelp" />
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <h2 className="display-4 pb-4">Register</h2>
+                <div className="mb-3 ">
+                    <label for="registerEmail" className="form-label">Your Email...</label>
+                    <input type="email" className="form-control" id="registerEmail" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div class="mb-3">
-                    <label for="registerPassword" class="form-label">Your password...</label>
-                    <input type="password" class="form-control" id="registerPassword" />
+                <div className="mb-3">
+                    <label for="registerPassword" className="form-label">Your password...</label>
+                    <input type="password" className="form-control" id="registerPassword" />
                 </div>
-                <div class="mb-3">
-                    <label for="registerConfirmPassword" class="form-label">Confirm password...</label>
-                    <input type="password" class="form-control" id="registerConfirmPassword" />
+                <div className="mb-3">
+                    <label for="registerConfirmPassword" className="form-label">Confirm password...</label>
+                    <input type="password" className="form-control" id="registerConfirmPassword" />
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="registerNewspaper" />
-                    <label class="form-check-label" for="registerNewspaper">Sign up for exclusive updates, discounts, new arrivals, contests, and more!</label>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="registerNewspaper" />
+                    <label className="form-check-label" for="registerNewspaper">Sign up for exclusive updates, discounts, new arrivals, contests, and more!</label>
                 </div>
-                <button type="submit" class="btn btn-lg btn-light">Create Account</button>
+                <button type="submit" className="btn btn-lg btn-light">Create Account</button>
             </form>
         </div>
     );
