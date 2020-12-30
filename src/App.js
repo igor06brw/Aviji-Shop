ba5e303fcd63e587fb96c1a38f8a60fcaa10ee13
Igor <igor06brw@gmail.com>
2020-12-30 18:56:37 +0100

    convert func app to class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 
 import './App.css';
@@ -10,24 +10,26 @@ import Auth from "./components/Auth/Auth";
 import Layout from "./Layout/Layout";
 
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Layout>
-          <Header />
-            <Switch>
-              <Route exact path="/" component={Products} />
-              <Route path="/product-detail" component={ProductDetail} />
-              <Redirect from="/register" to="/auth" />
-              <Redirect from="/login" to="/auth" />
-              <Route path="/auth" component={Auth} />
-            </Switch>
-          <Footer />
-        </Layout>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+            <Header description="xxx"/>
+            <Layout>
+              <Switch>
+                <Route exact path="/" component={Products} />
+                <Route path="/product-detail" component={ProductDetail} />
+                <Redirect from="/register" to="/auth" />
+                <Redirect from="/login" to="/auth" />
+                <Route path="/auth" component={Auth}/>
+              </Switch>
+            </Layout>
+            <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
