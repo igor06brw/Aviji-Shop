3fee86bca37f0d6cdf33746f033b933d5694f7b1
Igor <igor06brw@gmail.com>
2021-01-02 16:38:57 +0100

    on hover add additional template

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import card1 from '../../../assets/card/1.jpg'
 const Product = (props) => {
-    return (
-        <div className="col mb-4">
-            <div className="card">
+    const [hover, setHover] = useState(false);
+
+    let onHoverInfo = <img src={card1} className="card-img-top image-fit" alt="..."/>
+    
+    if(hover === true) {
+        onHoverInfo = (
+            <div>
                 <img src={card1} className="card-img-top image-fit" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{props.product.name}</h5>
                     <p className="card-text">{props.product.type}</p>
                 </div>
+            </div>
+        )
+    }
+
+
+
+    return (
+        <div className="col mb-4">
+            <div className="card" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                {onHoverInfo}
             </div>
         </div>
     );
