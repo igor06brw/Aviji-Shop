ce85977a2c1bdd0c9467845441f2cc87da868b00
Igor <igor06brw@gmail.com>
2021-01-03 17:03:48 +0100

    add buttons of add to cart and details

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import card1 from '../../../assets/card/1.jpg'
 import '../../../styles/Product.css'
+import '../../../styles/Customize/Custom__Animate.css'
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
     const [hover, setHover] = useState(false);
@@ -25,6 +27,8 @@ const Product = (props) => {
             <div className="card position-relative" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                 <img src={card1} className="card-img-top image-fit" alt="..."/>
                 <h5 className="position-absolute font-weight-bold text-dark m-1">${props.product.price}</h5>
+                <Link to="/product/:id" className="position-absolute product-details btn btn-secondary m-1">Details</Link>
+                <Link className="position-absolute product-cart btn btn-danger m-1">Add to cart</Link>
                 {onHoverInfo}
             </div>
         </div>
