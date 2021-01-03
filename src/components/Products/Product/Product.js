d9ebd7f422cf40e90805e1ca4d3750ce63b0c114
Igor <igor06brw@gmail.com>
2021-01-03 16:23:02 +0100

    add animate.css

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -9,7 +9,7 @@ const Product = (props) => {
     
     if(hover === true) {
         onHoverInfo = (
-            <div className="text-of-absolute">
+            <div className="text-of-absolute animate__animated animate__fadeInDown">
                 <div className="position-absolute card-body col bg-light border border-top-0">
                     <h5 className="card-title">{props.product.name}</h5>
                     <p className="card-text">{props.product.type}</p>
@@ -24,7 +24,7 @@ const Product = (props) => {
         <div className="col mb-4">
             <div className="card position-relative" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                 <img src={card1} className="card-img-top image-fit" alt="..."/>
-                <p className="position-absolute text-teal-900 fw-bold">{props.product.price}</p>
+                <h5 className="position-absolute font-weight-bold text-dark m-1">${props.product.price}</h5>
                 {onHoverInfo}
             </div>
         </div>
