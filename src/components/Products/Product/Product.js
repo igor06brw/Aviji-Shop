0480a64181c27315b3d255e2b425956056142307
Igor <igor06brw@gmail.com>
2021-01-03 15:43:24 +0100

    fix position of absolute

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -5,12 +5,11 @@ import '../../../styles/Product.css'
 const Product = (props) => {
     const [hover, setHover] = useState(false);
 
-    let onHoverInfo = <img src={card1} className="card-img-top image-fit" alt="..."/>
+    let onHoverInfo;
     
     if(hover === true) {
         onHoverInfo = (
-            <div className="position-relative text-of-absolute">
-                <img src={card1} className="card-img-top image-fit" alt="..."/>
+            <div className="text-of-absolute">
                 <div className="position-absolute card-body col bg-light border border-top-0">
                     <h5 className="card-title">{props.product.name}</h5>
                     <p className="card-text">{props.product.type}</p>
@@ -23,7 +22,9 @@ const Product = (props) => {
 
     return (
         <div className="col mb-4">
-            <div className="card" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <div className="card position-relative" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                <img src={card1} className="card-img-top image-fit" alt="..."/>
+                <p className="position-absolute text-teal-900 fw-bold">{props.product.price}</p>
                 {onHoverInfo}
             </div>
         </div>
