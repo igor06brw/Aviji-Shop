aed2cb126d661644b9f63d1396f244f9379048ef
Igor <igor06brw@gmail.com>
2021-01-03 18:04:15 +0100

    fix of images, move to public

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import card1 from '../../../assets/card/1.jpg'
 import '../../../styles/Product.css'
 import '../../../styles/Customize/Custom__Animate.css'
 import { Link } from 'react-router-dom';
 
 const Product = (props) => {
+    console.log(props)
     const [hover, setHover] = useState(false);
 
     let onHoverInfo;
@@ -25,7 +25,7 @@ const Product = (props) => {
     return (
         <div className="col mb-4">
             <div className="card position-relative" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-                <img src={card1} className="card-img-top image-fit" alt="..."/>
+                <img src={props.product.image} className="card-img-top w-100" alt="..."/>
                 <h5 className="position-absolute font-weight-bold text-dark m-1">${props.product.price}</h5>
                 <Link to="/product/:id" className="position-absolute product-details btn btn-secondary m-1">Details</Link>
                 <Link className="position-absolute product-cart btn btn-danger m-1">Add to cart</Link>
--- src/components/data/API.js
+++ src/components/data/API.js
@@ -1,11 +1,11 @@
 const OutfitAPI = {
     outfits: [
-        {id: 0, type: 't-shirt', name: 'T-shirt with number', favourite: false, price: 1.10},
-        {id: 1, type: 't-shirt', name: 'T-shirt with red shadow', favourite: false, price: 33.11},
-        {id: 2, type: 't-shirt', name: 'T-shirt Avengers', favourite: true, price: 67},
-        {id: 3, type: 't-shirt', name: 'T-shirt Reserved', favourite: false, price: 3.13},
-        {id: 4, type: 't-shirt', name: 'T-shirt House', favourite: false, price: 8.10},
-        {id: 5, type: 't-shirt', name: 'T-shirt Diverse', favourite: true, price: 7.10},
+        {id: 0, type: 't-shirt', name: 'T-shirt with number', favourite: false, price: 1.10, image: '/images/img_1.jpg'},
+        {id: 1, type: 't-shirt', name: 'T-shirt with red shadow', favourite: false, price: 33.11, image: '/images/img_2.jpg'},
+        {id: 2, type: 't-shirt', name: 'T-shirt Avengers', favourite: true, price: 67, image: '/images/img_3.jpg'},
+        {id: 3, type: 't-shirt', name: 'T-shirt Reserved', favourite: false, price: 3.13, image: '/images/img_4.jpg'},
+        {id: 4, type: 't-shirt', name: 'T-shirt House', favourite: false, price: 8.10, image: '/images/img_5.jpg'},
+        {id: 5, type: 't-shirt', name: 'T-shirt Diverse', favourite: true, price: 7.10, image: '/images/img_6.jpg'},
     ],
 
     allOutfits: function() {
