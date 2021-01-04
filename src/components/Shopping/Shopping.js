38eb1a489e7c3b0d8bf963a0af000e4883297277
Igor <igor06brw@gmail.com>
2021-01-04 18:16:03 +0100

    add example of table for orders

--- src/components/Shopping/Shopping.js
+++ src/components/Shopping/Shopping.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import ShoppingItem from './ShoppingItem/ShoppingItem';
+import ShoppingOrder from './ShoppingOrder/ShoppingOrder';
 
 class Shopping extends Component {
     render() {
         return (
-            <div>
-               Shopping 
+            <div className="container">
+                <table class="table table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col col-lg-2">Product</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Qty.</th>
+                            <th scope="col">Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <ShoppingItem />
+                    </tbody>
+                </table>
+               <ShoppingOrder />
             </div>
         );
     }
--- src/components/Shopping/ShoppingItem/ShoppingItem.js
+++ src/components/Shopping/ShoppingItem/ShoppingItem.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const ShoppingItem = () => {
     return (
-        <div>
-            
-        </div>
+        <tr>
+            <td scope="row">Example Tshirt</td>
+            <td>$64.23</td>
+            <td>1</td>
+            <td>Delete</td>
+        </tr>
     );
 }
 
