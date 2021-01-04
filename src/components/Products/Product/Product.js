941af496824044b44886b767aa90deb3583339c0
Igor <igor06brw@gmail.com>
2021-01-04 02:52:39 +0100

    add spinner until loading (setTimeout for fake)

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../styles/Product.css'
 import '../../../styles/Customize/Custom__Animate.css'
 import { Link } from 'react-router-dom';
+import Spinner from '../../Spinner/Spinner'
 
 const Product = (props) => {
     console.log(props)
     const [hover, setHover] = useState(false);
+    const [isLoading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false) ;
+        }, 300)
+    }, []);
 
     let onHoverInfo;
-    
+
     if(hover === true) {
         onHoverInfo = (
             <div className="text-of-absolute animate__animated animate__fadeInDown">
@@ -22,15 +30,20 @@ const Product = (props) => {
 
 
 
+
     return (
         <div className="col mb-4">
-            <div className="card position-relative" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-                <img src={props.product.image} className="card-img-top w-100" alt="..."/>
-                <h5 className="position-absolute font-weight-bold text-dark m-1">${props.product.price}</h5>
-                <Link to="/product/:id" className="position-absolute product-details btn btn-secondary m-1">Details</Link>
-                <Link className="position-absolute product-cart btn btn-danger m-1">Add to cart</Link>
-                {onHoverInfo}
-            </div>
+            {
+                !isLoading ? <div className="card position-relative" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                    <img src={props.product.image} className="card-img-top w-100" alt="..."/>
+                    <h5 className="position-absolute font-weight-bold text-dark m-1">${props.product.price}</h5>
+                    <Link to="/product/:id" className="position-absolute product-details btn btn-secondary m-1">Details</Link>
+                    <Link className="position-absolute product-cart btn btn-danger m-1">Add to cart</Link>
+                    {onHoverInfo}
+                    </div> 
+                : 
+                    <Spinner />
+            }
         </div>
     );
 }
