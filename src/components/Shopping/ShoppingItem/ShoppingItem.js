b2a5eba12334aa6c5405baf33a427687d1eae82e
Igor <igor06brw@gmail.com>
2021-01-07 23:35:23 +0100

    add css to center text and less width

--- src/components/Shopping/ShoppingItem/ShoppingItem.js
+++ src/components/Shopping/ShoppingItem/ShoppingItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../../styles/ShoppingItem.css'
 
 const ShoppingItem = (props) => {
     console.log(props);
     return (
         <tr>
             <td headers="product" scope="row">
-                <Link to={`/product/${1}`}>
-                    <img src={props.shoppingCart.image}/>
-                    <h6>{props.shoppingCart.name}</h6>
+                <Link className="d-flex align-self-center shopping-item-link" to={`/product/${1}`}>
+                    <img className="shopping-item-image align-self-center" src={props.shoppingCart.image}/>
+                    <h6 className="text-info pl-4">{props.shoppingCart.name}</h6>
                 </Link>
             </td>
             <td headers="price">{props.shoppingCart.price}</td>
