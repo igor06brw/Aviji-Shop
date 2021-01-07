698d1d7a3c734a1e484fef7cdbb8b38d2bf7c98f
Igor <igor06brw@gmail.com>
2021-01-07 23:06:11 +0100

    modify initialState: add quantity for example

--- src/reducers/shopping.js
+++ src/reducers/shopping.js
@@ -1,8 +1,8 @@
 
 const initialState = [
-    {id: 0, type: 't-shirt', name: 'T-shirt with Batman', favourite: false, price: 1.10, image: '/images/tshirt_1.jpg'},
-    {id: 5, type: 'shirt', name: 'Shirt with dots', favourite: true, price: 53, image: '/images/shirt_1.jpg'},
-    {id: 8, type: 't-shirt', name: 'T-shirt Atari', favourite: true, price: 32.45, image: '/images/tshirt_4.jpg'}
+    {id: 0, type: 't-shirt', name: 'T-shirt with Batman', favourite: false, quantity: 2, price: 1.10, image: '/images/tshirt_1.jpg'},
+    {id: 5, type: 'shirt', name: 'Shirt with dots', favourite: true, quantity: 5, price: 53, image: '/images/shirt_1.jpg'},
+    {id: 8, type: 't-shirt', name: 'T-shirt Atari', favourite: true, quantity: 1, price: 32.45, image: '/images/tshirt_4.jpg'}
 ]
 
 
