f50b52a73c06c0dd97479a49cad229618bc711e8
Igor <igor06brw@gmail.com>
2021-01-08 01:57:23 +0100

    init productdetail

--- src/components/Products/ProductDetails/ProductDetails.js
+++ src/components/Products/ProductDetails/ProductDetails.js
@@ -3,8 +3,13 @@ import React from 'react';
 const ProductDetail = (props) => {
     console.log(props);
     return (
-        <div>
-            <h1>Product Detail {props.type}</h1>
+        <div className="container">
+            <div className="detail-container col">
+                <img src={props.image} alt={props.type}/>
+            </div>
+            <div className="detail-container col">
+                <h3 className="detail-heading">{props.name}</h3>
+            </div>
         </div>
     );
 }
