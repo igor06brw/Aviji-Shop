ede00f028ac6511fd5b68ba8a58a875f10cd400d
Igor <igor06brw@gmail.com>
2021-01-08 02:16:09 +0100

    finish style of heading, desc, img and price

--- src/components/Products/ProductDetails/ProductDetails.js
+++ src/components/Products/ProductDetails/ProductDetails.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import '../../../styles/ProductDetail.css'
 
 const ProductDetail = (props) => {
     console.log(props);
     return (
-        <div className="container">
-            <div className="detail-container col">
-                <img src={props.image} alt={props.type}/>
-            </div>
-            <div className="detail-container col">
-                <h3 className="detail-heading">{props.name}</h3>
+        <div className="container my-4">
+            <div className="row">
+                <div className="detail-container col">
+                    <img src={props.image} alt={props.type}/>
+                </div>
+                <div className="detail-container col">
+                    <h3 className="detail-heading mb-5 text-center">{props.name}</h3>
+                    <p className="detail-description">{props.description}</p>
+                    <p className="detail-price mx-2">Price: <span>${props.price}</span></p>
+                </div>
             </div>
         </div>
     );
