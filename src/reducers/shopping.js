3e8c990e8bcf7ff782d9a682e78657e7879bfb4a
Igor <igor06brw@gmail.com>
2021-01-10 02:16:03 +0100

    delete initialState for shopping

--- src/reducers/shopping.js
+++ src/reducers/shopping.js
@@ -1,16 +1,9 @@
 
-const initialState = [
-    {id: 0, type: 't-shirt', name: 'T-shirt with Batman', favourite: false, quantity: 2, price: 1.10, image: '/images/tshirt_1.jpg'},
-    {id: 5, type: 'shirt', name: 'Shirt with dots', favourite: true, quantity: 5, price: 53, image: '/images/shirt_1.jpg'},
-    {id: 8, type: 't-shirt', name: 'T-shirt Atari', favourite: true, quantity: 1, price: 32.45, image: '/images/tshirt_4.jpg'}
-]
-
-
-const shoppingReducer = (state = initialState, action) => {
+const shoppingReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TO_SHOPPING_LIST': 
             {   
-                console.log(action.payload);
+                console.log(action.payload, 'IS ADDED');
                 state.push(action.payload);
             }
         default:
