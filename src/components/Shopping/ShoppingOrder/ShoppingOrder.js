180577a19edd324419d93447c658cbb157815ec7
Igor <igor06brw@gmail.com>
2021-01-12 23:40:13 +0100

    order summary of shopping list

--- src/components/Shopping/ShoppingOrder/ShoppingOrder.js
+++ src/components/Shopping/ShoppingOrder/ShoppingOrder.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const ShoppingOrder = (props) => {
     return (
-        <div>
-           ${props.summary}
+        <div className="bg-light text-center mb-4">
+            <h2 className="bg-secondary py-2">Summary: </h2>
+            <h3 className="h3 py-2 text-info">${props.summary}</h3>
         </div>
     );
 }
