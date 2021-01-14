4387c83d80b9d11d7ce1ab1bc31603ae50d4cc8e
Igor <igor06brw@gmail.com>
2021-01-14 01:57:38 +0100

    1/2 solve of delete issue

--- src/reducers/shopping.js
+++ src/reducers/shopping.js
@@ -3,14 +3,8 @@ const shoppingReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TO_SHOPPING_LIST': 
                 return [...state, action.payload];
-        case 'REMOVE_FROM_SHOPPING_LIST':
-                let isDeleted = false;
-                return [...state.slice(state.filter(item => {
-                    (item.id !== action.payload.id && !isDeleted) ? isDeleted = true : console.log('x'); 
-                    
-                }), 1)]
-
-                return [...state.splice()]
+        case 'REMOVE_FROM_SHOPPING_LIST': 
+                return  state.filter(e => { return e.id !== action.payload.id} )
         default:
             return state;
     }
