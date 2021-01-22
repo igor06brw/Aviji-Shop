dfbe673481992c00d954ac5dde1816bf54e802b0
Igor <igor06brw@gmail.com>
2021-01-22 15:27:02 +0100

    add compose for applyMiddleware

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,9 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import allReducers from "./reducers/";
 import thunk from "redux-thunk";
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const configureStore = (initialState) => {
-    return createStore(allReducers, initialState, applyMiddleware(thunk));
+    return createStore(
+        allReducers, 
+        initialState, 
+        composeEnhancer(applyMiddleware(thunk)));
 }
 
 export default configureStore;
