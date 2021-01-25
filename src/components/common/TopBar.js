6ff54a6e92259e0585f83d69ae82a593eed3008a
Igor <igor06brw@gmail.com>
2021-01-25 13:35:30 +0100

    modify of login/logout current user

--- src/components/common/TopBar.js
+++ src/components/common/TopBar.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 
 const Topbar = (props) => {
 
     const shopping = useSelector(state => state.shopping)
-    // useEffect(() => {
-    //     effect
-    //     return () => {
-    //         cleanup
-    //     };
-    // }, []);
+    const currentUser = useSelector(state => state.currentUser)
+
+    
 
     return (
         <nav className="navbar bg-dark">
@@ -18,13 +15,27 @@ const Topbar = (props) => {
                 <span className="mr-2">Currency: GBP</span>
                 <i className="fas fa-sort-down"></i>
             </Link>
-            <Link to="/auth" className="nav-link text-light">Sign in or Register</Link>
-            <Link to="/shopping" className="nav-link text-light bg-success" aria-disabled="true">
-                <i className="fas fa-shopping-cart"></i>
-                { 
-                    shopping.length === 0 ? <span className="empty ml-2">Empty</span> : <span className="empty ml-2 font-bold">( {shopping.length} )</span> 
-                 }
-            </Link>
+            {
+                (currentUser === null) ? (
+                    <>
+                        <Link to="/auth" className="nav-link text-light">Sign in or Register</Link>
+                        <Link to="/auth" className="nav-link text-light bg-success" aria-disabled="true">
+                            <i className="fas fa-shopping-cart"></i>
+                            <span className="empty ml-2">Login</span>
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/auth" className="nav-link text-light">Logout</Link>
+                        <Link to="/shopping" className="nav-link text-light bg-success" aria-disabled="true">
+                        <i className="fas fa-shopping-cart"></i>
+                        { 
+                            shopping.length === 0 ? <span className="empty ml-2">Empty</span> : <span className="empty ml-2 font-bold">( {shopping.length} )</span> 
+                        }
+                        </Link>
+                    </>
+                )
+            }
         </nav>
     );
 }
