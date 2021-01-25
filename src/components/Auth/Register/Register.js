a44187e2db07f183175a3b441dbb6a8bea9780ed
Igor <igor06brw@gmail.com>
2021-01-25 10:02:45 +0100

    add react-redux register in register comp

--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, { createRef } from 'react';
+import { connect } from "react-redux";
+import { register } from "../../../redux/actions/firebase"
 
-const Register = () => {
+const Register = ({ register, history }) => {
+    const email = createRef();
+    const password = createRef();
+    const confirmPassword = createRef();
+
+    const handleSubmit = event => {
+        
+        event.preventDefault();
+        console.log(password.current.value, confirmPassword.current.value)
+        if(password.current.value === confirmPassword.current.value) {
+            register(email.current.value, password.current.value)
+            .then(() => {
+                history.push("/")
+            })
+            .catch(error => {
+                console.error(error.message)
+            })
+        } else {
+            console.error('Not equal password!')
+        }
+    }
+
+
     return (
         <div className="p-5 col">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2 className="display-4 pb-4">Register</h2>
                 <div className="mb-3 ">
                     <label for="registerEmail" className="form-label">Your Email...</label>
-                    <input type="email" className="form-control" id="registerEmail" aria-describedby="emailHelp" />
+                    <input 
+                        name="email"    
+                        type="email" 
+                        className="form-control" 
+                        id="registerEmail"
+                        ref={email} 
+                        aria-describedby="emailHelp" 
+                    />
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label for="registerPassword" className="form-label">Your password...</label>
-                    <input type="password" className="form-control" id="registerPassword" />
+                    <input 
+                        name="password"
+                        type="password" 
+                        className="form-control" 
+                        id="registerPassword" 
+                        autoComplete="none"
+                        ref={password}
+                    />
                 </div>
                 <div className="mb-3">
                     <label for="registerConfirmPassword" className="form-label">Confirm password...</label>
-                    <input type="password" className="form-control" id="registerConfirmPassword" />
+                    <input 
+                        name="confirmPassword"
+                        type="password" 
+                        className="form-control" 
+                        id="registerConfirmPassword"
+                        autoComplete="none"
+                        ref={confirmPassword} />
                 </div>
                 <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="registerNewspaper" />
@@ -28,4 +72,4 @@ const Register = () => {
     );
 }
 
-export default Register;
+export default connect(null, { register })(Register)
