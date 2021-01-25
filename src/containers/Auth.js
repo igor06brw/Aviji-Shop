95b280d42cd16587934a6c3773328945b6c1e610
Igor <igor06brw@gmail.com>
2021-01-25 11:54:29 +0100

    add ternary condition for change templates

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import Register from '../components/Auth/Register/Register';
 import Login from '../components/Auth/Login/Login';
+import { connect } from 'react-redux';
 
 class Auth extends Component {
-    state = {
-        description: 'Sign in or Register'
-    };
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            description: 'Sign in or Register',
+        }
+    }
+
+    onAuthorizedUser() {
+        console.log(this.props.uid);
+        return this.props.uid !== undefined ? <><Login history={this.props.history}/><Register history={this.props.history}/></> : <p>LOGOUT</p>;
+        
+    }
 
 
 
     render() {
-        console.log(this.props)
         return (
             <div className="d-flex justify-content-center container">   
-                <Login history={this.props.history}/>
-                <Register history={this.props.history}/>
+                {this.onAuthorizedUser()}
             </div>
         );
     }
 }
 
-export default Auth;
+const mapStateToProps = state => (state.currentUser)
+
+export default connect(mapStateToProps)(Auth);
